10db61abfd9d89f370c3c6f8f72b901d34554651
ristianah76 <76610835+ristianah76@users.noreply.github.com>
2020-12-26 19:24:19 +0700

    Add files via upload

---
+++ atm_program.py
@@ -0,0 +1,43 @@
+from customer import Customer
+
+while True:
+    id = int(input("Masukkan pin anda: "))
+    trial = 0
+
+while (id !: int(atm.checkPin()) and trial < 3):
+    id = int(input("Pin anda salah. Silahkan Masukkan lagi: "))
+    trial += 1
+
+    if trial == 3:
+        print("Error. Silahkan ambil kartu dan coba lagi")
+        exit()
+
+while True:
+        print("Selamat Datang")
+    id = int(input("Pilih transaksi anda: "))
+
+    if id = 1
+        print("Cek Saldo")
+        checkBalance()
+
+    elif id = 2
+        print("Debet")
+        input(nominal < saldo)
+        withdrawBalance()
+            print("Sisa saldo: " + str(self.debet))
+
+    elif id = 3
+        print("Simpan")
+        depositBalance()
+            print("Saldo saat ini: ") + str(self.simpan))
+
+    elif id = 4
+        print("Ganti pin")
+        input("Masukkan pin baru anda: ") + str(self.newPin))
+        self.newPin != self.custPin
+
+    elif id = 5
+        print("Keluar")
+            random(int(100000 - 1000000) + str(datetime) + str(depositBalance))
+
+    else out()
